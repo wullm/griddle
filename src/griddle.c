1894de056be2ea7acaf496125662fcff80fc8b48
Willem Elbers <willem.h.elbers@durham.ac.uk>
2022-09-11 10:00:07 +0100

    Initial commit

---
+++ src/griddle.c
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * This file is part of griddle.
+ * Copyright (c) 2022 Willem Elbers (whe@willemelbers.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <assert.h>
+#include <complex.h>
+#include <mpi.h>
+#include <fftw3-mpi.h>
+
+#include "../include/griddle.h"
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printf("No parameter file specified.\n");
+        return 0;
+    }
+
+    /* Initialize MPI for distributed memory parallelization */
+    MPI_Init(&argc, &argv);
+    fftw_mpi_init();
+
+    /* Get the dimensions of the cluster */
+    int rank, MPI_Rank_Count;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &MPI_Rank_Count);
+
+    /* Read options */
+    const char *fname = argv[1];
+    if (rank == 0) {
+        message(rank, "\n");
+        header(rank, "Welcome to griddle; please wait a little.\n");
+        message(rank, "The parameter file is '%s'\n", fname);
+    }
+
+    /* Timer */
+    struct timeval time_stop, time_start;
+    gettimeofday(&time_start, NULL);
+    
+    /* Main griddle structuress */
+    struct params pars;
+        
+    /* Read parameter file for parameters, units, and cosmological values */
+    readParams(&pars, fname);
+    
+    /* Store the MPI rank */
+    pars.rank = rank;
+
+    /* Done with MPI parallelization */
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
+
+    /* Clean up */
+    cleanParams(&pars);
+
+    /* Timer */
+    gettimeofday(&time_stop, NULL);
+    long unsigned microsec = (time_stop.tv_sec - time_start.tv_sec) * 1000000
+                           + time_stop.tv_usec - time_start.tv_usec;
+    message(rank, "\nTime elapsed: %.5f s\n", microsec/1e6);
+
+}
