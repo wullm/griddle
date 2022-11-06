434d6c1426d11e47b11dc8fee7c223c3bc4b0164
Willem Elbers <willem.h.elbers@durham.ac.uk>
2022-11-06 05:14:38 +0000

    Add missing definition of the relativistic drift correction

---
+++ include/relativity.h
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * This file is part of Sedulus.
+ * Copyright (c) 2022 Willem Elbers (whe@willemelbers.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+#ifndef RELATIVITY_H
+#define RELATIVITY_H
+
+#include "units.h"
+#include "particle.h"
+
+/* Relativistic equations of motion (2207.14256) */
+static inline double relativistic_drift(const struct particle *p,
+                                        const struct physical_consts *pcs,
+                                        double a) {
+    if (p->type == 6) {
+        double v2 = p->v[0] * p->v[0] + p->v[1] * p->v[1] + p->v[2] * p->v[2];
+        double ac = a * pcs->SpeedOfLight;
+        double ac2 = ac * ac;
+        return ac / sqrt(ac2 + v2);
+    } else {
+        return 1.0;
+    }
+}
+
+#endif
